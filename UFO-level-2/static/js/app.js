df873bb81cb373a46578da0d00d32e5dde027987
SugeilyMG <73979000+SugeilyMG@users.noreply.github.com>
2021-06-13 17:49:24 -0500

    Add files via upload

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -1,4 +1,34 @@
 // from data.js
 var tableData = data;
+var tbody = d3.select("tbody");
+var button = d3.select("button");
+var form = d3.select("form");
+
+
 
 // YOUR CODE HERE!
+tableData.forEach((ufos) => {
+    var row = tbody.append("tr");
+    Object.entries(ufos).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+
+button.on("click", runEnter);
+form.on("submit",runEnter);
+
+function runEnter() {
+  d3.event.preventDefault();
+
+  var inputElement = d3.select("#datetime");
+
+  var inputValue = inputElement.property("value");
+
+  var table = document.getElementById("#ufo-table");
+
+  var filteredData = tableData.filter(record => record.datetime === inputValue)
+  console.log(filteredData);
+
+  
+};
